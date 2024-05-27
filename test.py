5e90c97099a0d9f7be34a565b3dc13f666664271
Eládio Alves <eladiolink1@gmail.com>
2024-05-27 17:50:34 -0300

    adição

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from Grids_Search_Models.Logistic_Regression import grid_logistic_regression
 from Utils.PreProcessing import preProcessing
 from Utils.BackTranslation import dataframeWithBackTranslation
 from Grids_Search_Models.XgBoost import grid_xgBoost
+from Grids_Search_Models.AdaBoost import grid_adaBoost
 from Utils.Ros import ROS
 from Utils.Rus import RUS
 from Utils.PreProcessing import Stemming
@@ -19,6 +20,6 @@ vectorizer = TfidfVectorizer()
 X_train = vectorizer.fit_transform(X_train)
 X_test = vectorizer.transform(X_test)
 
-grid_xgBoost(X_train, y_train, X_test, y_test, "TEST")
+# grid_xgBoost(X_train, y_train, X_test, y_test, "TEST")
 
-
+grid_adaBoost(X_train, y_train, X_test, y_test, "TEST")
